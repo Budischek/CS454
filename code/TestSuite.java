69d7ba9ed7719a0510392752d9db471a24cd639e
Lolcodes <lolcodes1337@gmail.com>
2018-11-29 22:05:34 +0900

    added new Test-Runner class and modified script for single testcase

--- code/TestSuite.java
+++ code/TestSuite.java
@@ -3,55 +3,56 @@ import org.junit.Test;
 
 // ****** Test cases: Strings of length 10 ******
 public class TestSuite {
+
+
+  Class0 test0 = new Class0();
+  Class1 test1 = new Class1();
+  boolean bug0 = false;
+  boolean bug1 = false;
+
   @Test
-  public void evaluatesExpression() {
-
-    Class0 test0 = new Class0();
-    boolean bug0 = false;
-
+  public void evaluatesExpression0() {
     // ****** method0 test cases ******
-    bug0 = test0.method0("aboklatesd"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method0("aboklatesd"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method0("aicobmsjqw"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method0("aicobmsjqw"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method0("helloworld"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method0("helloworld"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method0("hyowodxxsb"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method0("hyowodxxsb"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
 
     // ****** method1 test cases ******
-    bug0 = test0.method1("bcoadgasds"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method1("bcoadgasds"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method1("mhmsdtyoax"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method1("mhmsdtyoax"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method1("herroyouuh"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method1("herroyouuh"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
-    bug0 = test0.method1("lpokiqmcte"); System.out.printf("\nbug0=%b", bug0);
+    bug0 = test0.method1("lpokiqmcte"); System.out.printf("bug0=%b\n", bug0);
     // assertEquals(false, bug0);
+  }
 
-
-    Class1 test1 = new Class1();
-    boolean bug1 = false;
-
+  @Test
+  public void evaluatesExpression1() {
     // ****** method0 test cases ******
-    bug1 = test1.method0("xyoklatesd"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method0("xyoklatesd"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method0("xdcppmsjqw"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method0("xdcppmsjqw"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method0("helloworld"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method0("helloworld"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method0("hyowodxxsb"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method0("hyowodxxsb"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
 
     // ****** method1 test cases ******
-    bug1 = test1.method1("bcoadgasds"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method1("bcoadgasds"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method1("womsdtyoax"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method1("womsdtyoax"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method1("heplyooihm"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method1("heplyooihm"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-    bug1 = test1.method1("lpokiqmcte"); System.out.printf("\nbug1=%b", bug1);
+    bug1 = test1.method1("lpokiqmcte"); System.out.printf("bug1=%b\n", bug1);
     // assertEquals(false, bug1);
-
   }
 }
